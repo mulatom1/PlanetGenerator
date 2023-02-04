d148fb5f6fa5bdd5399d2e40e5c3c46f91886d7d
Piotr Pawlowski <ai2yume@gmail.com>
2023-02-04 11:42:15 +0100

    fixed front; generate.jsx life cycle component fix

--- FRONT/src/components/Generate.jsx
+++ FRONT/src/components/Generate.jsx
@@ -1,97 +1,70 @@
-import React, { Component } from 'react'
-import { useState, useEffect } from 'react';
+import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
-import GenText from './GenText.jsx';
-import Footer from './Footer.jsx';
-import '/src/styles/Generate.css'
+import GenText from "./GenText.jsx";
+import Footer from "./Footer.jsx";
+import "/src/styles/Generate.css";
 import {
-    MouseParallaxChild,
-    MouseParallaxContainer
-  } from "react-parallax-mouse";
+  MouseParallaxChild,
+  MouseParallaxContainer,
+} from "react-parallax-mouse";
 
 const Generate = () => {
-    //  React.useEffect(() => {
-    //     console.log('MyComponent onMount');
-    //     return () => {
-    //       console.log('MyComponent onUnmount');
-    //     };
-    // }, []);
-    //   function getRandomInt() {
-    // console.log(Math.floor(Math.random() * 1000))
-    //   }
-    //   getRandomInt()
-    // const [data, setData] = React.useState([])
-    const [spinner, setSpinner] = useState(true);
-    const [loading, setLoading] = useState(null);
-    const API = "http://tomsoft1dev1.northeurope.cloudapp.azure.com:8000/get_planet"
-  
+  const [spinner, setSpinner] = useState(true);
+  const [imageUrlisFired, setImageUrlisFired] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+  const API = "http://tomsoft1dev1.northeurope.cloudapp.azure.com:8000/get_planet";
 
-   const FolderImages = [
-    '/assets/images/examples/1.jpg',
-    '/assets/images/examples/2.jpg'
-   ]
-    const [currentImageIndex, setCurrentImageIndex] = useState(Math.floor(Math.random() * FolderImages.length))
+  if (imageUrlisFired === false) {
+    setTimeout(() => {
+      const time = Date.now();
+      setImageUrl(API + "/?data=" + time);
+      setImageUrlisFired(true);
+      setSpinner(false);
+    }, 2000)
+  }
 
-    const time = Date.now()
-    const imageUrl = API + "/?data=" + time;
+  return (
+    <div>
+      <MouseParallaxContainer
+        globalFactorX={0.1}
+        globalFactorY={0.1}
+        containerStyle={{ overflow: "inherit" }}
+      >
+        <div className="hero__stars-bg">
+          <MouseParallaxChild
+            factorX={0.43}
+            factorY={-0.3}
+            containerStyle={{ zindex: "3", position: "inherit" }}
+          >
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+          </MouseParallaxChild>
+        </div>
 
-    const changeImage = () => {
-    const randomNumber = Math.floor(Math.random() * FolderImages.length);
-      setCurrentImageIndex(randomNumber);
-    }
-    useEffect(() => changeImage(), [])
-
-
-    React.useEffect(() => {
-  
-    setTimeout(() => setSpinner(false), 12000)
-    setTimeout(() => setLoading(true), 8000)
-      });
-
-    return <div>
-         <MouseParallaxContainer globalFactorX={0.1} globalFactorY={0.1} containerStyle={{overflow: "inherit",}}>
-            <div className="hero__stars-bg">
-            <MouseParallaxChild factorX={.43} factorY={-0.3} containerStyle={{zindex: "3",
-      position:"inherit"}}>
-                <div id='stars'></div>
-                <div id='stars2'></div>
-                <div id='stars3'></div>
-                </MouseParallaxChild>
-            </div>
-            
-            
-            <div className="App hero__section--container">
-                <div className="hero__section--two-columns two-left">
-                    <GenText/>
-                </div>
-                <div className="hero__section--two-columns two-right">
-                {loading ? null :
-          <div>
-          <img src={"assets/images/epochs64_3.gif"}/>
-          <p style={{position:"relative", bottom:"0"}}>Modeluję...</p>
+        <div className="App hero__section--container">
+          <div className="hero__section--two-columns two-left">
+            <GenText />
           </div>
-        }
-        <img className="generated_image"
-          style={loading ? {} : {display: 'none',  width:'512px'}}
-          src={imageUrl}
-            onLoad={() => setTimeout(()=>setLoading(true),2000)}
-        />
+          <div className="hero__section--two-columns two-right">
+            {!spinner ? (
+              <div>{<img className="generated_image"
+                style={{ width: '512px' }}
+                src={imageUrl}></img>}</div>
 
-              
-            {/* <img
-            style={loading ? {} : {display: 'none'}}
-            src={imageUrl}
-            onLoad={() => setLoading(true)}
-            /> */}
-                </div>
-            </div>
-            </MouseParallaxContainer>
-            <Footer/>
-           </div>
-        }
+            ) : (
+              <div>
+                <img src="assets/images/epochs64_3.gif"></img>
+                <p style={{position: "relative", bottom: "0" }}>Modeluję i pobieram...</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </MouseParallaxContainer>
+      <Footer />
+    </div>
+  );
+};
 
-    export default Generate;
-
-
-    
-
+export default Generate;
